db2d776a3a4442785628c6daafbd1842523f8238
Tobias Gleiter <gleiter.tobias@outlook.de>
2025-06-02 19:39:02 -0400

    fix: rm text color

--- README.md
+++ README.md
@@ -10,8 +10,8 @@ Vona makes it easier for developers and entrepreneurs to validate their ideas wi
 
 ### 🔧 Vona Blocks (Landing Ready)
 
-- [x] Header (with logo and menu)
-- [x] Hero section
+- [ ] Header (with logo and menu)
+- [x] Hero section (Headline, Description, 2x CTA Buttons)
 - [ ] Email signup form
 - [ ] Feature list (e.g. “Why Vona”)
 - [ ] Call to action (CTA) section
--- dist/blocks/hero.js
+++ dist/blocks/hero.js
@@ -94,7 +94,7 @@ class VonaBlockHero extends HTMLElement {
     <div class="relative isolate px-6 pt-14 lg:px-8">
         <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
             <div class="text-center">
-                <h1 class="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
+                <h1 class="text-5xl font-semibold tracking-tight text-balance sm:text-7xl">
                     <slot name="headline"></slot>
                 </h1>
                 <p class="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
